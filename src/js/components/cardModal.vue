64b885bdcbb27aacf338d07b6165f58a30a75565
Saloni Robinson <saloni@Salonis-MBP.connect>
2019-08-22 22:18:41 +0100

    add card-modal component

--- src/js/components/cardModal.vue
+++ src/js/components/cardModal.vue
@@ -1,9 +1,16 @@
 <template>
 <div
-  v-if="showing"
+  v-if="showing" v-on:click.self="close"
   class="fixed inset-0 w-full h-screen flex items-center justify-center bg-semi-75"
 >
-  <div class="w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
+  <div class="relative w-full max-w-2xl bg-white shadow-lg rounded-lg p-8">
+      <button
+        aria-label="close"
+        class="absolute top-0 right-0 text-xl text-gray-500 my-2 mx-4" 
+        @click.prevent="close"
+      >
+        ×
+      </button>
     <slot />
   </div>
 </div>
@@ -16,6 +23,11 @@ export default {
             required: true,
             type: Boolean
         }
+    },
+    methods: {
+      close() {
+        this.$emit('close');
+      }
     }
 }
 </script>
